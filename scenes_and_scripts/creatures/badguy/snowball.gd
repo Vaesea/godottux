845d1b3a057f0a7e50d76ca216f4f34428263bfa
Vaesea <159406785+Vaesea@users.noreply.github.com>
2026-04-06 09:20:32 +0100

    Some more changes
    - Add all enemy code to badguy.gd (new enemy.gd)
    - Add enemy template
    - Add Spiky (including Sleeping Spiky)

--- scenes_and_scripts/creatures/badguy/snowball.gd
+++ scenes_and_scripts/creatures/badguy/snowball.gd
@@ -1,38 +1,6 @@
 extends BadGuy
 
 func _ready() -> void:
+	basic_walking = true
+	freezable = false
 	super()
-	$Image.play("walk")
-	$TuxDetector.area_entered.connect(_on_tux_detector_area_entered)
-	$TuxDetector.body_entered.connect(_on_tux_detector_body_entered)
-	kill_other_enemies = false
-	
-func _physics_process(delta: float) -> void:
-	super(delta)
-
-func _on_tux_detector_area_entered(area) -> void:
-	if area.is_in_group("Stomp"):
-		# TODO: Prevent player from being damaged by enemy below when on slope
-		if area.get_parent().get_real_velocity().y > 0:
-			death(false)
-			print(":3")
-			area.get_parent().stomp_bounce()
-	 
-	if area.is_in_group("StupidThing") and area.get_parent().kill_other_enemies:
-		death(true)
-	elif area.is_in_group("StupidThing") and area.get_parent().kill_self_on_touching_enemy:
-		death(true)
-		area.get_parent().death(true)
-
-func _on_tux_detector_body_entered(body) -> void:
-	if body.is_in_group("Player") and not dead:
-		body.damage()
-	elif body.is_in_group("FireBullet") and not dead:
-		body.queue_free()
-		death(true)
-
-func move():
-	if not dead:
-		velocity.x = direction * speed
-	else:
-		velocity.x = 0
---
+++ scenes_and_scripts/creatures/badguy/spiky.gd
@@ -0,0 +1,7 @@
+extends BadGuy
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	spiky = true
+	smart = true
+	super()
